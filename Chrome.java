ea3533fec73e3ed18578bb66fc94acf65059fe8f
karthikeyanjayaramu18 <karthikeyanjayaramu18@gmail.com>
2025-11-09 08:40:55 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+				System.out.println(version);
+		
+		String developer = "Google";
+		        System.out.println(developer);
+		        
+		        boolean isbeta = false;
+		        System.out.println(isbeta);
+		        
+		        int releaseYear = 2008;
+		        System.out.println("releaseYear of type int with a value of" +" "+releaseYear);
+		        
+		        char shortcutKey = 'C';
+		        System.out.println(shortcutKey);
+		        
+		
+
+	}
+
+}
